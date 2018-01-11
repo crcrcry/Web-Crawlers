187e2ad1669a361e54dec8b11091d9fcf1778062
crcrcry <870247639@qq.com>
2018-01-11 21:10:12 +0800

    Wiki-editors: step 1

--- wiki-editors/README.md
+++ wiki-editors/README.md
@@ -2,8 +2,13 @@
 - 维基百科 Wikipedia Editors 数据爬虫
 
 ## 原理
-- 维基百科 Special pages 中包含维基的数据统计，包括 All pages 和 All users
+- 维基百科 Special pages 中包含维基的数据统计，包括 All pages 和 All users（分红名与非红名）
 - 通过 All users 获取每位 user 的贡献信息
 
-## 缺陷
-- 维基百科的请求速度太慢，平均 4-5 秒才能抓 500 个用户，更换方法，直接下载 wikidata
+## wikidata
+- 维基百科的请求速度太慢，平均每秒请求一次页面
+- wikidata 2018-1 有 28 G 数据，且只包含 wiki 中的条目和属性，不包含用户数据，无法使用
+
+## improve
+- 先做第一步好了
+- 第二部找个框架来实现并行
--- wiki-editors/main.py
+++ wiki-editors/main.py
@@ -2,49 +2,101 @@ import requests
 import re
 
 # 爬取的数据地址
-UsersListURL = 'https://en.wikipedia.org/w/index.php?title=Special:ListUsers&limit=500'
-UserDetailURL = 'https://en.wikipedia.org/wiki/Special:Contributions/'
+UsersListURL = 'https://en.wikipedia.org/wiki/Special:ListUsers?username=&group=&editsOnly=1&wpsubmit=&wpFormIdentifier=mw-listusers-form&limit=500'
+UserContribsURL = 'https://en.wikipedia.org/wiki/Special:Contributions/'
+# example: https://en.wikipedia.org/wiki/Special:Contributions/Yin?start=2015-01-01&end=2015-12-31
 
 # 网页 HTML 信息抓取
 def getWebHTML(url):
   res = requests.get(url)
   return res.text
 
+def handleHTMLEscapeChar(str):
+  return str.replace('&quot;', '"').replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&nbsp;', ' ')
+
 # 用户列表获取
 def getUsers():
   try:
     # 正则写的比较简单，但应该 work
     regexr = {
-      'pageUsers': re.compile('<bdi>(.+?)</bdi>'),
-      'nextPage': re.compile('<a href="([^"]+)" class="mw-nextlink" title="Special:ListUsers" rel="next">next 500</a>')
+      'normalUsers': re.compile(r'class="mw-userlink" title="[^"]+"><bdi>(.+?)</bdi>'),
+      'redUsers': re.compile(r'class="new mw-userlink" title="[^"]+"><bdi>(.+?)</bdi>'),
+      'nextPage': re.compile(r'<a href="([^"]+)" class="mw-nextlink" title="Special:ListUsers" rel="next">next 500</a>')
     }
 
-    users = []
-
+    normalUsers = []
+    redUsers = []
     nextPageURL = UsersListURL
     while True:
       pageHTML = getWebHTML(nextPageURL)
-      pageUsers = regexr['pageUsers'].findall(pageHTML)
-      users.extend(pageUsers)
-      print(len(users))
+
+      # normal users，并处理 HTML 转义字符，逐步写入文件
+      pageNormalUsers = regexr['normalUsers'].findall(pageHTML)
+      normalUsers.extend([handleHTMLEscapeChar(u) for u in pageNormalUsers])
+      fileStr = ''
+      for u in pageNormalUsers:
+        fileStr += u + '\n'
+      with open('./normal_users.txt', 'a') as f:
+        f.write(fileStr)
+
+      # red users，并处理 HTML 转义字符，逐步写入文件
+      pageRedUsers = regexr['redUsers'].findall(pageHTML)
+      redUsers.extend([handleHTMLEscapeChar(u) for u in pageRedUsers])
+      fileStr = ''
+      for u in pageRedUsers:
+        fileStr += u + '\n'
+      with open('./red_users.txt', 'a') as f:
+        f.write(fileStr)
+
+      # print info
+      print('Normal Users: %d, Red Users: %d' % (len(normalUsers), len(redUsers)))
 
       nextPageURL = regexr['nextPage'].findall(pageHTML)[0]
-      nextPageURL = nextPageURL.replace('&amp;', '&')
+      nextPageURL = handleHTMLEscapeChar(nextPageURL)
       nextPageURL = 'https://en.wikipedia.org' + nextPageURL
-      print(nextPageURL)
   except IndexError as e:
     print('Not found next page url')
-
-  return users
+  
+  return {'redUsers': redUsers, 'normalUsers': normalUsers}
     
-
 # 用户信息获取
-def getUsersDetails(users):
-  pass
+def getUsersContribs(users):
+  # 正则写的比较简单，但应该 work
+  regexr = {
+    'contribs': re.compile(r'<li data-mw-revid="\d+"><a href="[^"]+" class="[^"]+" title="[^"]+">([^<]+)</a>'),
+  }
+
+  usersContribs = {}
+  for u in users:
+    # URL 拼装与 HTML 爬取
+    # test
+    pageHTML = getWebHTML(UserContribsURL + u + '?start=2006-01-01&end=2016-12-31')
+    # 正则，获取其中的 contributions
+    contribs = regexr['contribs'].findall(pageHTML)
+    print(contribs)
+    # 存储每个人的所有 contributions，没有的存储 None
+    if len(contribs) == 0:
+      usersContribs[u] = None
+    else:
+      usersContribs[u] = contribs
+  
+  return usersContribs
 
 # 获取数据
 def getData():
-  uesrs = getUsers()
+  # 用户列表
+  users = getUsers()
+  print('Finish Getting Users List.')
+
+  # 贡献时间列表
+  # contribs = getUsersContribs(users)
+  # print('Finish Getting Contributions List')
+  # with open('./contribs.txt', 'w') as f:
+  #   str = ''
+  #   for c in contribs:
+  #     for t in contribs[c]:
+  #       str = str + c + ', ' + t + '\n'
+  #   f.write(str)
 
 # Main 接口
 def main():

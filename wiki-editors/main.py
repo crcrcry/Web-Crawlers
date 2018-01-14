7fe7b250d4b47924d1b30afe944bd0ca8a14f3a8
crcrcry <870247639@qq.com>
2018-01-15 00:04:55 +0800

    性能与可用性测试

--- wiki-editors/main.py
+++ wiki-editors/main.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import time
 
 # 爬取的数据地址
 UsersListURL = 'https://en.wikipedia.org/wiki/Special:ListUsers?username=&group=&editsOnly=1&wpsubmit=&wpFormIdentifier=mw-listusers-form&limit=500'
@@ -11,10 +12,11 @@ def getWebHTML(url):
   res = requests.get(url)
   return res.text
 
+
 def handleHTMLEscapeChar(str):
   return str.replace('&quot;', '"').replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&nbsp;', ' ')
 
-# 用户列表获取
+# 用户列表获取，直接存于文件或数据库中（防止内存耗尽）
 def getUsers():
   try:
     # 正则写的比较简单，但应该 work
@@ -24,42 +26,46 @@ def getUsers():
       'nextPage': re.compile(r'<a href="([^"]+)" class="mw-nextlink" title="Special:ListUsers" rel="next">next 500</a>')
     }
 
-    normalUsers = []
-    redUsers = []
+    # 维基 3000 万用户，用户数组消耗内存会超级大，直接一边算一边存文件里
+    redUsersNum = 0
+    normalUsersNum = 0
     nextPageURL = UsersListURL
     while True:
+      t0 = time.time()
       pageHTML = getWebHTML(nextPageURL)
+      t1 = time.time()
 
       # normal users，并处理 HTML 转义字符，逐步写入文件
       pageNormalUsers = regexr['normalUsers'].findall(pageHTML)
-      normalUsers.extend([handleHTMLEscapeChar(u) for u in pageNormalUsers])
+      normalUsersNum += len(pageNormalUsers)
       fileStr = ''
       for u in pageNormalUsers:
-        fileStr += u + '\n'
+        fileStr += handleHTMLEscapeChar(u) + '\n'
       with open('./normal_users.txt', 'a') as f:
         f.write(fileStr)
 
       # red users，并处理 HTML 转义字符，逐步写入文件
       pageRedUsers = regexr['redUsers'].findall(pageHTML)
-      redUsers.extend([handleHTMLEscapeChar(u) for u in pageRedUsers])
+      redUsersNum += len(pageRedUsers)
       fileStr = ''
       for u in pageRedUsers:
-        fileStr += u + '\n'
+        fileStr += handleHTMLEscapeChar(u) + '\n'
       with open('./red_users.txt', 'a') as f:
         f.write(fileStr)
-
-      # print info
-      print('Normal Users: %d, Red Users: %d' % (len(normalUsers), len(redUsers)))
-
+      
       nextPageURL = regexr['nextPage'].findall(pageHTML)[0]
       nextPageURL = handleHTMLEscapeChar(nextPageURL)
       nextPageURL = 'https://en.wikipedia.org' + nextPageURL
+
+      t2 = time.time()
+      # print info
+      print('Normal Users: %d, Red Users: %d, Request time: %.2fs, Regexr time: %.2fs' % (normalUsersNum, redUsersNum, (t1 - t0), (t2 - t1)))
+
   except IndexError as e:
     print('Not found next page url')
   
-  return {'redUsers': redUsers, 'normalUsers': normalUsers}
     
-# 用户信息获取
+# 用户信息获取，直接存于文件中
 def getUsersContribs(users):
   # 正则写的比较简单，但应该 work
   regexr = {
@@ -70,7 +76,7 @@ def getUsersContribs(users):
   for u in users:
     # URL 拼装与 HTML 爬取
     # test
-    pageHTML = getWebHTML(UserContribsURL + u + '?start=2006-01-01&end=2016-12-31')
+    pageHTML = getWebHTML(UserContribsURL + u + '?start=2016-01-01&end=2016-12-31')
     # 正则，获取其中的 contributions
     contribs = regexr['contribs'].findall(pageHTML)
     print(contribs)
@@ -85,8 +91,10 @@ def getUsersContribs(users):
 # 获取数据
 def getData():
   # 用户列表
-  users = getUsers()
-  print('Finish Getting Users List.')
+  t0 = time.time()
+  getUsers()
+  t1 = time.time()
+  print('Finish Getting Users List in %.2fs.' % (t1 - t0))
 
   # 贡献时间列表
   # contribs = getUsersContribs(users)
